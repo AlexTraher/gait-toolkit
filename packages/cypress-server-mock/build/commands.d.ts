fa01546578f7d071177156c25bf57a40d6db62ea
Alex Traher <alex.traher@gmail.com>
2022-11-04 11:34:17 +0000

    updating package versions

--- packages/cypress-server-mock/build/commands.d.ts
+++
@@ -1,8 +0,0 @@
-import { MockConfig } from "./types";
-declare global {
-    namespace Cypress {
-        interface Chainable {
-            serverMock(config: MockConfig): Chainable;
-        }
-    }
-}
